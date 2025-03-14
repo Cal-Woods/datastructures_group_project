f312764fdff8d75e9b4e93da700c45d654f916fb
Cal Farrell Woods <153258375+Cal-Woods@users.noreply.github.com>
2025-03-14 23:01:33 +0000

    Feature: Added getter methods to User.
    getUsername() returns User username field and getPassword() returns User password field.
    
    Methods have public access-modifiers to allow outside access but only what the method allows.

--- src/main/java/business/User.java
+++ src/main/java/business/User.java
@@ -4,4 +4,17 @@ public class User {
     private String username;
     private String password;
 
+    //Getters
+    /**
+     * Gets username of User object instance.
+     * 
+     * @return Username field of User instance.
+     */
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
 }
