aefdb63cebaed9f4f9dc4a8fe802255e464f6df7
Cal Farrell Woods <153258375+Cal-Woods@users.noreply.github.com>
2025-03-14 23:19:57 +0000

    Docs: Edited javadocs on getter methods and added new javadoc to class User.

--- src/main/java/business/User.java
+++ src/main/java/business/User.java
@@ -1,5 +1,8 @@
 package business;
 
+/**
+ * Records basic user information.
+ */
 public class User {
     private String username;
     private String password;
@@ -8,12 +11,21 @@ public class User {
     /**
      * Gets username of User object instance.
      * 
-     * @return Username field of User instance.
+     * @return User Username field of instance.
+     * 
+     * @author Cal Woods
      */
     public String getUsername() {
         return this.username;
     }
 
+    /**
+     * Gets password field of User instance.
+     * 
+     * @return User password field of instance object.
+     * 
+     * @author Cal Woods
+     */
     public String getPassword() {
         return this.password;
     }
