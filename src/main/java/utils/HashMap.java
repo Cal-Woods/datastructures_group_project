70bb0f2c1b6854c412772ae30b375405d7aed1ff
Cal Woods <D00270955@student.dkit.ie>
2025-04-07 21:15:12 +0100

    Feature: Added subclsss Entry.
    Entry is a wrapper for incoming HashMap data. A key:User-object pair.
    
    Contains two attributes: String key and User user.

--- src/main/java/utils/HashMap.java
+++ src/main/java/utils/HashMap.java
@@ -1,5 +1,7 @@
 package utils;
 
+import business.User;
+
 /**
  * An implementation of a HashMap structure.
  * 
@@ -20,5 +22,27 @@ public class HashMap {
     public HashMap() {
         this.slotLists = new LinkedList[INITIAL_SIZE];
         this.count = 0;
+    }
+
+    /**
+    * A wrapper class for HashMap key:value pairs.
+    */
+    private static class Entry {
+        //Attributes
+        private String key;
+        private User user;
+
+        //Constructor
+        public Entry(String key, User user) {
+            if(key == null) {
+                throw new NullPointerException("Given key was null which is NOT allowed!");
+            }
+            if(user == null) {
+                throw new NullPointerException("Given User object was null which is NOT allowed!");
+            }
+
+            this.key = key;
+            this.user = user;
+        }
     }
 }
