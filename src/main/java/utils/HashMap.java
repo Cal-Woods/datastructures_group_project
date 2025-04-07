4ccedb42dc1be0a3a9e2511be084c188e99f1a16
Cal Woods <D00270955@student.dkit.ie>
2025-04-07 20:22:34 +0100

    Feature: Added attributes and basic HashMap constructor.
    Constructor sets attributes to starting values
    count becomes 0 and slotList of array of LinkedList objects gets initialised with 10 spaces in array.

--- src/main/java/utils/HashMap.java
+++ src/main/java/utils/HashMap.java
@@ -1,5 +1,16 @@
 package utils;
 
 public class HashMap {
-    
+    //Class variable
+    private static final int INITIAL_SIZE = 10;
+
+    //Instance attributes
+    private int count;
+    private LinkedList[] slotLists;
+
+    //Constructors
+    public HashMap() {
+        this.slotLists = new LinkedList[INITIAL_SIZE];
+        this.count = 0;
+    }
 }
