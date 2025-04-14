e7fbd7e70ff19c3087de7394a9000e417f98726b
D00258712 <d00258712@student.dkit.ie>
2025-04-14 15:40:22 +0100

    Feature: Add method LinkedList - Collins

--- src/main/java/utils/LinkedList.java
+++ src/main/java/utils/LinkedList.java
@@ -3,8 +3,8 @@ package utils;
 public class LinkedList {
     private int numElements;
 
-    private node first;
-    private node last;
+    private Node first;
+    private Node last;
 
     /**
      * No-argument constructor initialising the list to default settings.
@@ -15,8 +15,41 @@ public class LinkedList {
         this.numElements = 0;
     }
 
+    /**
+     * Size Method for linkedlist
+     * @return
+     */
     public int size(){
         return numElements;
+    }
+
+    /**
+     * Add the specified element to the end of this list.
+     * @param value the element to be added to this list
+     * @throws IllegalArgumentException if the value to be added is null
+     */
+    public void add(String value){
+        // VALIDATION
+        if(value == null){
+            throw new IllegalArgumentException("Null cannot be added to list");
+        }
+        // SET UP:
+        // Wrap incoming data in node
+        Node newNode = new Node(value);
+
+        // LOGIC:
+        // Deal with adding first element to list
+        // If the list does not already have a value / is empty
+        if(first == null){
+            // Set the first element in the list to be the new node
+            first = newNode;
+            last = newNode;
+        }else{
+            last.next = newNode;
+            last = newNode;
+        }
+
+        numElements++;
     }
 
 
@@ -24,11 +57,11 @@ public class LinkedList {
 
 
 
-public static class node {
+public static class Node {
     private String data;
-    private node next;
+    private Node next;
 
-    public node(String data) {
+    public Node(String data) {
         this.data = data;
         this.next = null;
     }
