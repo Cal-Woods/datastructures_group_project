18f4235895339af06ca054a8489db112fabcd06c
Cal Woods <D00270955@student.dkit.ie>
2025-04-15 20:33:49 +0100

    Started main program.

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,20 @@
+package app;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap users = 
+    }
+
+
+    private static void initUsersMap(String fileLoc) {
+        //Validation
+        
+        //Declare reference to file for storing users
+        File usersFile= new File(fileLoc);
+        //Scanner readUsers = new Scanner(usersFile);
+    }
+}
