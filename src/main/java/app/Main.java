fdf8ac30d294b4bf4233b2f3633005073700adc6
Cal Woods <D00270955@student.dkit.ie>
2025-04-17 11:35:28 +0100

    Update: Added initUserMap() method for initialising a HashMap instance.
    Uses java.io.File & java.util.Scanner for reading file objects. Will creatre the file if it does not exist.
    
    Throws FileNotFoundException & IOException exceptions.

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,20 +1,60 @@
 package app;
 
 import java.io.File;
-import java.util.HashMap;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import utils.HashMap;
 import java.util.Scanner;
 
+import business.User;
+
 public class Main {
-    public static void main(String[] args) {
-        HashMap users = 
-    }
+    //Class variables
+    private static final String USER_STORE_DIR = "persistent-data";
+    private static final String USER_STORE_FILE = USER_STORE_DIR+"/Users";
+    public static void main(String[] args) throws IOException {
+        HashMap users = initUsersMap(USER_STORE_DIR, USER_STORE_FILE);
+
+        for (int i = 0; i < 1; i++) {
+            
+        }
+}
 
 
-    private static void initUsersMap(String fileLoc) {
+    private static HashMap initUsersMap(String fileDir, String fileName) throws FileNotFoundException, IOException {
         //Validation
         
-        //Declare reference to file for storing users
-        File usersFile= new File(fileLoc);
-        //Scanner readUsers = new Scanner(usersFile);
+
+        //Declare HashMap temp as future return type
+        HashMap temp = new HashMap();
+
+        //Declare references to file path for storing users
+        File usersDir = new File(fileDir);
+        File usersFile= new File(fileName);
+
+        if(!usersFile.exists()){
+            //Create directory
+            usersDir.mkdir();
+            //Create specified file if it does not exist
+            usersFile.createNewFile();
+        }
+
+        //Check if file is empty
+        if(usersFile.length() == 0.00) {
+            return temp;
+        }
+        
+        //Set-up  Scanner  and set delimiter
+        Scanner readUsers = new Scanner(usersFile);
+        readUsers.useDelimiter(":");
+
+        //Initialise while loop to iterate until there are no more tokens in Scanner
+        while(readUsers.hasNext()) {
+            temp.put(new User(readUsers.next(), readUsers.next()));
+            readUsers.nextLine();
+        }
+
+
+        return temp;
     }
 }
