ab6428715a2c1e81614787ad7c32e628bb7517da
Cal Woods <D00270955@student.dkit.ie>
2025-04-17 17:34:42 +0100

    Update: Added three methods to class.
    size(), isEmpty() & get() methods.
    
    size() returns the number of elements in the dynamicArray instance, isEmpty() returns whether or not there are more than 0 elements, get(int pos) takes an int & validates it before returning the User element associated withh that Entry.

--- src/main/java/utils/dynamicArray.java
+++ src/main/java/utils/dynamicArray.java
@@ -1,5 +1,6 @@
 package utils;
 
+import business.User;
 import utils.Entry;
 
 /**
@@ -8,4 +9,51 @@ import utils.Entry;
 public class dynamicArray {
     private int numElements;
     private Entry[] array;
+    
+    private static final int STARTING_SIZE = 10;
+
+    //Constructors
+    /**
+     *Initialises a dynamicArray instance with 0 numElements & an Entry array of size STARTING_SIZE == 10
+     */
+    public dynamicArray() {
+        this.numElements = 0;
+        this.array = new Entry[STARTING_SIZE];
+    }
+
+    /**
+     * Gets current number of elements in dynamicArray instance.
+     * 
+     * @return Number of elements in instance
+     */
+    public int size() {
+        return this.numElements;
+    }
+
+    /**
+     * Gets whether or not dynamicArray instance is empty.
+     * 
+     * @return True if dynamicArray instance contains 0 elements, false otherwise
+     */
+    public boolean isEmpty() {
+        return this.numElements == 0;
+    }
+    
+    /**
+     * Gets an Entry object at a given position.
+     * @param pos Given position
+     * 
+     * @return An Entry object
+     */
+    public User get(int pos) {
+        //Validation
+        if(pos < 0) {
+            throw new IndexOutOfBoundsException("Given pos was "+pos+" which is an invalid place in a list, positive numbers only!");
+        }
+        if(pos >= this.numElements) {
+            throw new IndexOutOfBoundsException("Given pos was "+pos+" but there is ONLY "+this.numElements+" elements in this list!");
+        }
+
+        return this.array[pos].getValue();
+    }
 }
