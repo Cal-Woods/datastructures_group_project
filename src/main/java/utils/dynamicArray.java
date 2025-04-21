fefef3fbc638cb433a7d60b80962ac96c8689a79
Jerome <jeromewaudoj@gmail.com>
2025-04-21 09:56:30 +0100

    DynamicArray code with the remove method added in

--- src/main/java/utils/dynamicArray.java
+++ src/main/java/utils/dynamicArray.java
@@ -1,161 +1,114 @@
 package utils;
-
-import business.User;
-import utils.Entry;
-
-/**
- * A dynamic ArrayList to hold Entry objects
- */
+/**This is a dynamic arraylist that holds entry to objects**/
+/**It initializes a DynamicArray instance with 0 numElements $ an Entry array of size STARTING_ SIZE==10**/
 public class DynamicArray {
+    private static final int STARTING_SIZE = 10;
     private int numElements;
     private Entry[] array;
-    
-    private static final int STARTING_SIZE = 10;
 
-    //Constructors
-    /**
-     *Initialises a dynamicArray instance with 0 numElements & an Entry array of size STARTING_SIZE == 10
-     */
     public DynamicArray() {
         this.numElements = 0;
         this.array = new Entry[STARTING_SIZE];
     }
 
     /**
-     * Gets current number of elements in dynamicArray instance.
-     * 
-     * @return Number of elements in instance
-     */
+     * should get current number of elemnts on the list in the array
+     * should return the number of elemnts on the list
+     **/
+
     public int size() {
         return this.numElements;
     }
 
     /**
-     * Gets whether or not dynamicArray instance is empty.
-     * 
-     * @return True if dynamicArray instance contains 0 elements, false otherwise
-     */
+     * the method should check the list is empty
+     * the method should return true if the list is empty
+     * if not, the method should return false if the list is not empty
+     **/
     public boolean isEmpty() {
         return this.numElements == 0;
     }
-    
-    /**
-     * Gets an Entry object at a given position.
-     * @param pos Given position
-     * 
-     * @return An Entry object
-     */
-    public Entry get(int pos) {
-        //Validation
-        if(pos < 0) {
-            throw new IndexOutOfBoundsException("Given pos was "+pos+" which is an invalid place in a list, positive numbers only!");
-        }
-        if(pos >= this.numElements) {
-            throw new IndexOutOfBoundsException("Given pos was "+pos+" but there is ONLY "+this.numElements+" elements in this list!");
-        }
 
-        return this.array[pos];
+    /**
+     * get entry of an object from a specific position
+     *
+     * @param position specified position
+     *                 return the specified position on the list
+     *                 a validation will be used which will help us locate the object targeted on the list
+     **/
+
+    public Entry get(int position) {
+        if (position < 0 || position >= this.numElements) {
+            throw new IndexOutOfBoundsException();
+        }
+        return array[position];
     }
 
+
     /**
-     * Searches a dynamicArray instance for a given User object and gives the index of that object.
-     * 
-     * @param user Given User
-     * 
-     * @return The index of the User if found, -1 if the User was NOT found or if dynamicArray instance is empty
-     * 
-     * @throws NullPointerException If given User object was null
-     */
-    public int indexOf(User user) {
-        //Validation
-        if(user == null) {
-            throw new NullPointerException("Given User object was null!");
-        }
-        if(this.isEmpty()) {
-            System.out.println("This dynamicArray instance does NOT contain any elements.");
+     * search through the list for a given object and give the index of the object being searched
+     *
+     * @ user given user
+     * @ return the index of the user if found and if not, -1 if the user was not found or incase the list is empty
+     **/
 
-            return -1;
+    public int indexOf(Entry entry) {
+        if (entry == null) {
+            throw new NullPointerException("entry is null");
         }
-
-        //Initialise for loop to iterate until number of elements
-        for (int i = 0; i <  this.numElements; i++) {
-            //Check the incoming User for every User in array
-            if(user.equals(this.array[i].getValue())) {
+        for (int i = 0; i < this.numElements; i++) {
+            if (array[i] == entry) {
                 return i;
             }
         }
-
         return -1;
     }
 
     /**
-     * Adds a given User to the end of the list.
-     * @param user Given User
-     * 
-     * @throws NullPointerException If given User is null
-     */
-    public void add(User user) {
-        //Validation
-        if(user == null) {
-            throw new NullPointerException("Given User to add was null!");
+     * add a new entry
+     **/
+    public void add(Entry entry) {
+        if (entry == null) {
+            throw new NullPointerException("list is null");
         }
-
-        //If list numElements equals array.length
-        if(this.numElements == this.array.length) {
-            this.array = grow();
+        if (this.numElements == array.length) {
+            array = grow();
         }
-
-        //Add to end of list
-        this.array[this.numElements] = new Entry(user.getUsername(), user);
-
-        //Increment count by 1
-        this.numElements++;
+        array[numElements++] = new entry;
     }
 
-
-    public User remove(int pos) {
-        //Validation
-        validPos(pos);
-
-        //Shift delete element at given pos
-        //TODO: shiftDelete(pos);
-
-        return null;
-    }
-
-    //Private helper methods
     /**
-     * Grows the DynamicArray instance array by 10
-     * @return temp grown array
-     */
-    private Entry[] grow() {
-        //Declare temp Entry array
-        Entry[] temp = new Entry[this.numElements + STARTING_SIZE];
+     * removing entry from a list
+     *
+     * @param entry the index of the item to remove from the list
+     *              return removed item from the list
+     *              a validation will be used to check if the list is empty
+     **/
 
-        //Copy all Entry objects from this.array to temp
-        System.arraycopy(this.array, 0, temp, 0, this.numElements);
+    public void remove(Entry entry) {
+        if (entry == null) {
+            throw new IndexOutOfBoundsException("list is null");
+        }
+        entry removed = array[entry].getValue();
 
-        //Return temp
+        for (int i = 0; i < this.numElements - 1; i++) {
+            if (array[i] == entry) {
+                array[i] = array[i + 1];
+            }
+        }
+        array[numElements - 1] = null;
+
+    }
+    //private helper method
+    /** grows the dynamicarray instance array by 10
+     * @return temp grown away */
+    //declare temp Entry array
+    private Entry[] grow(){
+        Entry temp[] = new Entry[STARTING_SIZE];
+
+        //copy all entry objects from this.array to temp
+        System.arraycopy(array, 0, temp, 0, numElements);
+
         return temp;
-    }
-
-    /**
-     * Checks if a given position is valid in the DynamicArray instance.
-     * 
-     * @param pos Given position
-     * 
-     * @return true if given position is valid.
-     * 
-     * @throws IndexOutOfBoundsException If given position is < 0 or given position >= number of elements in list
-     */
-    private boolean validPos(int pos) {
-        if(pos < 0) {
-            throw new IndexOutOfBoundsException("Given pos was "+pos+" which is an invalid position in a list or array!");
-        }
-        if(pos >= this.numElements) {
-            throw new IndexOutOfBoundsException("Given pos was "+pos+" which is greater than the size of this list!");
-        }
-
-        return true;
     }
 }
