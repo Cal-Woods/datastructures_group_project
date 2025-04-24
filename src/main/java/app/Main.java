037f4a6aa0366c69350ab7f0ef5d174f280e8a6a
Cal Woods <D00270955@student.dkit.ie>
2025-04-24 15:23:03 +0100

    Update: Added method to store a User in a file and made multiple code changes to main method.
    storeUser(User user) uses a FileWriter write to write text to a given file.
    
    Will need to update initUsersMap() to read extra data from Agent entries in file.

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,31 +2,32 @@ package app;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
-import utils.HashMap;
-
 import java.util.Random;
 import java.util.Scanner;
 
-import business.Agent;
-import business.User;
+//Our code
+import business.*;
+import utils.*;
 
 public class Main {
     //Class variables
     private static final String USER_STORE_DIR = "persistent-data";
     private static final String USER_STORE_FILE = USER_STORE_DIR+"/Users";
-    
+
+    //Declare authenticatedUser
+    private static User authenticatedUser = null;
     public static void main(String[] args) throws IOException {
         HashMap users = initUsersMap(USER_STORE_DIR, USER_STORE_FILE);
+        PriorityQueue openTickets;
+        LinkedList closedTickets;
 
         //Start of program
         System.out.println("Welcome to the IT ticket management program! Once logged in, you may submit a ticket to the application, or if you are an Agent, you may manage tickets.\n");
         
         //Print message
         System.out.println("\nYou must login before you can access this application. Choose to login or register below.");
-
-        //Declare authenticatedUser
-        User authenticatedUser = null;
 
         //Declare running boolean
         boolean running = true;
@@ -36,31 +37,46 @@ public class Main {
 
         //Initialise while loop
         while (running) {
-            //Print options
-            System.out.println("Please choose an option?");
+            //If not authenticated
+            if(authenticatedUser == null) {
+                //Print options
+                System.out.println("Please choose an option?");
 
-            System.out.print("1) Login(If no users are registered, you will have to register)\n2) Register\nAny other key to exit\nOption: ");
+                System.out.print("1) Login(If no users are registered, you will have to register)\n2) Register\nAny other key to exit\nOption: ");
 
-            //Capture input
-            String option = keyboard.next();
-            
-            //Clear keyboard Scanner
-            keyboard.nextLine();
+                //Capture input
+                String option = keyboard.next();
+                
+                //Clear keyboard Scanner
+                keyboard.nextLine();
 
-            //Validate option
-            if(option.equals("1")) {
-                authenticatedUser = loginSys(users);
-            }
-            else if(option.equals("2")) {
-                boolean registerSuccess = registerSys(users);
-            }
-            else {
-                running = false;
-            }
+                //Declare boolean to confirm registration status
+                boolean registerSuccess = false;
+
+                //Validate option
+                if(option.equals("1")) {
+                    authenticatedUser = loginSys(users);
+                }
+                else if(option.equals("2")) {
+                    registerSuccess = registerSys(users);
+                }
+                else {
+                    System.out.println("\nYou have typed "+option+". Exiting the application...");
+                    running = false;
+                }
         }
 
-        //TODO - Collins, Jerome: Add data structures for storing open tickets, closed tickets: PriorityQueue, LinkedList
-}
+        
+            //Check if authenticatedUser is NOT null
+            else if(authenticatedUser !=  null) {
+                //TODO - Add all logic here
+
+                System.out.println("\n"+authenticatedUser);
+
+                return;
+            }
+        }
+    }
 
     /**
      * Initialises application HashMap by reading a file from a given directory path & a file name.
@@ -190,7 +206,16 @@ public class Main {
         return null;
     }
 
-
+    /**
+     * Registration system for users & agents.
+     * 
+     * @param compare Given HashMap
+     * 
+     * @return True if registration successful.
+     * 
+     * @auth
+     *  Cal Woods
+     */
     private static boolean registerSys(HashMap compare) {
         //Declare Scanner set to System.in
         Scanner scanner = new Scanner(System.in);
@@ -294,6 +319,62 @@ public class Main {
         else if(input.equalsIgnoreCase("user")) {
             //Place new User in given HashMap compare
             compare.put(username, new User(username, password));
+        }
+
+        //Store new User/Agent in filePath
+        return true;
+    }
+
+    /**
+     * Stores Users including Agents in a file with the given file path.
+     * 
+     * @param filePath Given file path
+     * @param dirPath Given Directory path
+     * 
+     * @author Cal Woods
+     * 
+     * @see Note To be used as helper for registerSys() method.
+     */
+    private boolean storeUser(String dirPath, String filePath, User user) throws FileNotFoundException, IOException{
+        //validation
+        if(filePath == null) {
+            //Print error message
+            System.err.println("\nGiven file path was null!");
+
+            return false;
+        }
+        if(dirPath == null) {
+            //Print error message
+            System.err.println("\nGiven file path was null!");
+
+            return false;
+        }
+
+        if(user == null) {
+            System.err.println("Given User object was null which is NOT allowed!");
+
+            return false;
+        }
+
+        //Check if given file path exists
+        if(!new File(dirPath).exists()) {
+            //Create fiile
+            new File(dirPath).mkdir();
+        }
+        
+        //Check if given file path exists
+        if(!new File(filePath).exists()) {
+            //Create fiile
+            new File(filePath).createNewFile();
+        }
+
+        //Declare FileWriter to write to file
+        FileWriter write = new FileWriter(new File((dirPath+"/"+filePath)));
+
+        write.write(user.getUsername()+":"+user.getPassword()+":");
+        //Check user type
+        if(user instanceof Agent) {
+            write.write(((Agent) user).getAgentName()+":"+((Agent) user).getAgentID()+":");
         }
         return true;
     }
